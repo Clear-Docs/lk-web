a9b127dd5bf98e7bb3aa1d824bdf9c4017ba1156
Anton Butov <butov6101@gmail.com>
2026-02-13 22:47:21 +0300

    chore(build): update project description and favicon - new ds

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -13,7 +13,8 @@ version = "1.0-SNAPSHOT"
 kobweb {
     app {
         index {
-            description.set("ClearDocs - Powered by Kobweb")
+            description.set("ClearDocs")
+            faviconPath.set("/favicon.png")
         }
     }
 }
